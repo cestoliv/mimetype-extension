7c60419be594c3e42bca749d9c4234c4714a3e2c
Olivier CARTIER <cestoliv@chevro.fr>
2020-05-26 16:54:09 +0200

    adding mimetype from MDN

--- mimetype-extension.js
+++ mimetype-extension.js
@@ -3,6 +3,10 @@ exports.get = (ext) => {
         //FILES
         case "html":
             return 'text/html'
+        case "htm":
+            return 'text/html'
+        case "xhtml":
+            return 'application/xhtml+xml'
         case "xml":
             return 'text/xml'
         case "webmanifest":
@@ -25,6 +29,50 @@ exports.get = (ext) => {
             return "application/vnd.amazon.ebook"
         case "bin":
             return "application/octet-stream"
+        case "csh":
+            return "application/x-csh"
+        case "csv":
+            return "text/csv"
+        case "doc":
+            return "application/msword"
+        case "docx":
+            return "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+        case "epub":
+            return "application/epub+zip"
+        case "ics":
+            return "text/calendar"
+        case "mpkg":
+            return "application/vnd.apple.installer+xml"
+        case "odp":
+            return "application/vnd.oasis.opendocument.presentation"
+        case "ods":
+            return "application/vnd.oasis.opendocument.spreadsheet"
+        case "odt":
+            return "application/vnd.oasis.opendocument.text"
+        case "ogx":
+            return "application/ogg"
+        case "ppt":
+            return "application/vnd.ms-powerpoint"
+        case "pptx":
+            return "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+        case "rtf":
+            return "application/rtf"
+        case "sh":
+            return "application/x-sh"
+        case "swf":
+            return "application/x-shockwave-flash"
+        case "ts":
+            return "application/typescript"
+        case "vsd":
+            return "application/vnd.visio"
+        case "xls":
+            return "application/vnd.ms-excel"
+        case "xlsx":
+            return "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        case "xml":
+            return "application/xml"
+        case "xul":
+            return "application/vnd.mozilla.xul+xml"
 
         //ARCHIVES
         case "arc":
@@ -33,6 +81,16 @@ exports.get = (ext) => {
             return "application/x-bzip"
         case "bz2":
             return "application/x-bzip2"
+        case "jar":
+            return "application/java-archive"
+        case "rar":
+            return "application/x-rar-compressed"
+        case "tar":
+            return "application/x-tar"
+        case "zip":
+            return "application/zip"
+        case "7z":
+            return "application/x-7z-compressed"
 
         //IMAGES
         case "png":
@@ -45,11 +103,19 @@ exports.get = (ext) => {
             return "image/jpeg"
         case "jpeg":
             return "image/jpeg"
+        case "gif":
+            return "image/gif"
+        case "tif":
+            return "image/tiff"
+        case "tiff":
+            return "image/tiff"
+        case "webp":
+            return "image/webp"
 
         //AUDIOS
-        case "audio-ogg":
+        case "oga":
             return "audio/ogg"
-        case "audio-webm":
+        case "weba":
             return "audio/webm"
         case "mp3":
             return "audio/mpeg"
@@ -57,22 +123,24 @@ exports.get = (ext) => {
             return "audio/wav"
         case "aac":
             return "audio/aac"
+        case "midi":
+            return "audio/midi"
+        case "mid":
+            return "audio/midi"
 
         //VIDEOS
         case "mp4":
             return "video/mp4"
         case "ogg":
-            console.log("[headers warning !] .get(\"ogg\") return a VIDEO header for the ogg format, you can specify .get(\"audio-ogg\") for an audio format and .get(\"video-ogg\") for a video format. Have a good day !")
             return "video/ogg"
-        case "video-ogg":
+        case "ogv":
             return "video/ogg"
         case "webm":
-            console.log("[headers warning !] .get(\"webm\") return a VIDEO header for the webm format, you can specify .get(\"audio-webm\") for an audio format and .get(\"video-webm\") for a video format. Have a good day !")
-            return "video/ogg"
-        case "video-webm":
             return "video/webm"
         case "avi":
             return "video/x-msvideo"
+        case "mpeg":
+            return "video/mpeg"
 
         //FONTS
         case "ttf":
@@ -80,9 +148,11 @@ exports.get = (ext) => {
         case "woff2":
             return 'application/font-woff2'
         case "eot":
-            return 'font/eot'
+            return 'application/vnd.ms-fontobject'
         case "woff":
             return 'application/font-woff'
+        case "otf":
+            return 'font/otf'
 
         //OTHER
         default:
